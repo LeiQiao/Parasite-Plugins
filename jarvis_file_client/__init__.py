beaa6f390c36791f20154bda801042a5d65fe7cd
lei.qiao <lei.qiao@chinapnr>
2019-07-02 09:34:33 +0800

    remove jarvis file sdk

--- jarvis_file_client/__init__.py
+++
@@ -1,2 +0,0 @@
-from .jarvis_file_client import JarvisFileClient
-from .jfile_sdk import JFile
--- jarvis_file_client/jarvis_file_client.py
+++
@@ -1,6 +0,0 @@
-from pa.plugin import Plugin
-
-
-class JarvisFileClient(Plugin):
-    __pluginname__ = 'jarvis_file_client'
-    pass
