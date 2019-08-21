731b187771e7fd6aacead77fc5e9d9c58f40fe47
LeiQiao <LeiQiaoTalk@hotmail.com>
2019-08-21 11:27:44 +0800

    去掉守护进程，直接创建工作进程

--- base_schedule/base_schedule_plugin.py
+++ base_schedule/base_schedule_plugin.py
@@ -2,7 +2,9 @@ from pa.plugin import Plugin
 import pa
 import sys
 from plugins.base import BasePlugin
-from .guardian import ScheduleGuardian
+from multiprocessing import Process
+from .schedule import Schedule
+from datetime import datetime
 
 
 class SchedulePlugin(Plugin):
@@ -11,8 +13,7 @@ class SchedulePlugin(Plugin):
     def __init__(self, plugin_name=None, manifest=None):
         super(SchedulePlugin, self).__init__(plugin_name, manifest)
 
-        self.guardian = ScheduleGuardian()
-        self.guardian.start()
+        self.schedule_jobs = {}
 
     @Plugin.after_load
     def regist_schedule_from_plugin_manifest(self):
@@ -31,7 +32,13 @@ class SchedulePlugin(Plugin):
             schedule_plugin.regist_schedule_job(schedule_id, cls, schedule_time)
 
     def regist_schedule_job(self, schedule_id, schedule_class, schedule_time):
-        self.guardian.regist_schedule_job(schedule_id, schedule_class, schedule_time)
+        if schedule_id in self.schedule_jobs:
+            return
+
+        job_process = Process(target=worker_process,
+                              args=(schedule_id, schedule_class, schedule_time))
+        job_process.start()
+        self.schedule_jobs[schedule_id] = job_process
 
     @staticmethod
     def _get_class_in_module(plugin, class_name):
@@ -45,3 +52,29 @@ class SchedulePlugin(Plugin):
             str(e)
             raise ModuleNotFoundError('unable found schedule class \'{0}\''.format(class_name))
         return cls
+
+
+def worker_process(schedule_id, work_class, schedule_time):
+    # 这里需要使用 __name__ 进行比较
+    if work_class.__base__.__name__ != Schedule.__name__:
+        raise ImportError('schedule class \'{0}\' is not a subclass of \'Schedule\''.format(work_class))
+
+    schedule = work_class()
+    schedule.start(schedule_time, schedule_id)
+    pa.log.info('start schedule \'{0}\''.format(schedule_id))
+
+    first_run = True
+    while True:
+        try:
+            schedule.run_times += 1
+            pa.log.debug('schedule {0} triggered at {1}, {2} times'
+                         .format(schedule.schedule_id, datetime.now(), schedule.run_times))
+            if first_run:
+                first_run = False
+                schedule.first_run()
+            else:
+                schedule.run()
+        except Exception as e:
+            pa.log.error('schedule {0} error: {1}'.format(schedule.schedule_id, e))
+
+        schedule.sleep()
