38782a1a430c5b56c507b83a84e88fe476ee1623
lei.qiao <lei.qiao@chinapnr>
2020-05-17 20:59:52 +0800

    修复数据库多线程时导致的问题，添加连接池回收时间一小时后所有连接池的连接重新建立数据库连接

--- base_db_manager/like_flask_sqlchemy.py
+++ base_db_manager/like_flask_sqlchemy.py
@@ -5,14 +5,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class SQLAlchemy:
     def __init__(self, db_uri):
-        self.engine = create_engine(db_uri, isolation_level='READ COMMITTED')
-        self.session = sessionmaker(bind=self.engine)()
+        self.engine = create_engine(db_uri, isolation_level='READ COMMITTED', pool_recycle=3600)
+        self.session_factory = sessionmaker(bind=self.engine)
+        self.session = scoped_session(self.session_factory)
         self.Model =declarative_base(
                 cls=Model,
                 name='Model',
                 metadata=None
             )
-        self.Model.query_class = self._create_scoped_session(Query)
+        self.Model.query_class = self.session
         self.Model.query = _QueryProperty(self)
         self.Column = Column
         self.String = String
@@ -24,11 +25,6 @@ class SQLAlchemy:
         self.relationship = relationship
         self.backref = backref
 
-    def _create_scoped_session(self, query_cls):
-        session = sessionmaker(autoflush=True, autocommit=False,
-                               bind=self.engine, query_cls=query_cls)
-        return scoped_session(session)
-
 class Model:
     query = None
     query_class = None
@@ -38,4 +34,4 @@ class _QueryProperty:
         self.sa = sa
 
     def __get__(self, obj, class_type):
-        return class_type.query_class.query(class_type)
+        return class_type.query_class().query(class_type)
