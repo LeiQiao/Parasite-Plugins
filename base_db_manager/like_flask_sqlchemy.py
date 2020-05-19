592b9b9699c52e2867b17e3509906b978f7fbbc1
lei.qiao <lei.qiao@chinapnr>
2020-05-19 12:46:40 +0800

    fix bugs?

--- base_db_manager/like_flask_sqlchemy.py
+++ base_db_manager/like_flask_sqlchemy.py
@@ -3,6 +3,21 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref, 
 from sqlalchemy.ext.declarative import declarative_base
 
 
+"""
+    like_flask_sqlalchemy
+    ~~~~~~~~~~~~~~~~
+
+    用于在非 flask 项目中像 flask_sqlalchemy 一样操作数据库
+
+    !重要，因为没有 flask 的请求规则，所以在具体的使用场景中(一般在多线程中使用较多)
+    记住一定要在数据库操作完毕(最好推迟到线程结束前)时调用下面语句用来释放数据库连接：
+
+    db.session.remove()
+
+    否则会出现以下类似错误：
+
+    QueuePool limit of size 5 overflow 10 reached, connection timed out, timeout 30
+"""
 class SQLAlchemy:
     def __init__(self, db_uri):
         self.engine = create_engine(db_uri, pool_recycle=3600)
@@ -26,8 +41,7 @@ class SQLAlchemy:
 
 
 class BaseQuery(Query):
-    def __del__(self):
-        self.session.close()
+    pass
 
 
 class Model:
