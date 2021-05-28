2982ca6626fd1e0109c29af304020809345c00c2
LeiQiao <LeiQiaoTalk@hotmail.com>
2021-05-28 11:18:05 +0800

    fix init with dict crash

--- record_api/editable_result.py
+++ record_api/editable_result.py
@@ -1,9 +1,12 @@
 class EditableResult(tuple):
     def __init__(self, result):
         super(EditableResult, self).__init__()
-        self._keys = result.keys()
-        for key in result.keys():
-            setattr(self, key, getattr(result, key))
+        self._keys = list(result.keys())
+        for key in self._keys:
+            if isinstance(result, dict):
+                setattr(self, key, result[key])
+            else:
+                setattr(self, key, getattr(result, key))
         self.startIndex = 0
 
     def __iter__(self):
@@ -16,9 +19,9 @@ class EditableResult(tuple):
         value = self._keys[self.startIndex]
         self.startIndex += 1
         return value
-    
+
     def set(self, key, value):
         if key not in self._keys:
             self._keys.append(key)
-            
+
         setattr(self, key, value)
